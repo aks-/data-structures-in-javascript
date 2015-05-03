66b967e99bd1c3e4aca0b2a20ca2ab55a3e68071
aks- <coderatlabs@gmail.com>
2015-05-03 10:44:11 +0530

    Mirror, addition and traversal methods for BST
    * add method to add items to BST
    * add method to traverse in postOrder and inOrder
    * add method to mirror the tree

---
+++ binary_search_tree.js
@@ -0,0 +1,58 @@
+var BST = function() {
+  this._root = null;
+}
+
+BST.prototype = {
+  add: function(item) {
+    var node = {
+      item: item,
+      right: null,
+      left: null
+    }
+
+    var current = this._root;
+
+    while (true) {
+      if (this._root == null) {
+        this._root = node;
+        break;
+      }
+
+      if (current.item > item) {
+        if (current.left == null) {
+          current.left = node;
+          break;
+        } else {
+          current = current.left;
+        }
+      } else if (current.right < item) {
+        if (current.right == null) {
+          current.right = node;
+          break;
+        } else {
+          current = current.right;
+        }
+      } else {
+        break;
+      }
+    }
+  },
+
+  inOrder: function(node, fn) {
+    if (node.left != null) {
+      this.inOrder(node.left, fn);
+    }
+    fn.call(node);
+    if (node.right != null) {
+      this.inOrder(node.right, fn);
+    }
+  },
+
+  traverseInorder: function() {
+    this.inOrder(this._root, function() {
+      console.log(this.item);
+    });
+  }
+}
+
+module.exports = BST;

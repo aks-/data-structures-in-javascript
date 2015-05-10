85a3f3e67e4a4639ea0638bf459a3287a8e65583
aks- <coderatlabs@gmail.com>
2015-05-10 16:19:41 +0530

    Add mergesort for linked list

--- algorithms/mergesort_on_linked_list.js
+++ algorithms/mergesort_on_linked_list.js
@@ -1,39 +1,119 @@
-var LinkedList = function() {
-    this._first = null;
-    this._last =null;
+var doublyLinkedList = require('../data-structures/doubly_linked_list');
+
+function sort() {
+    return mergeSort(this._first, this._last);
 }
 
-LinkedList.prototype = {
-    add: function(item) {
-        var node = {
-            item: item,
-            next: null,
-            prev: null
-        }
+function mergeSort(first, last) {
+    if (first == null) return null;
+    if (first == last) return {
+        item: first.item,
+        next: null,
+        prev: null
+    }
 
-        if (this._first == null) {
-            this._first = node;
-            this._last = node;
+    var middle = partition(first, last);
+    var leftPart = mergeSort(first, middle);
+    var rightPart = mergeSort(middle.next, last);
+    return merge(leftPart, rightPart);
+}
+
+function partition(first, last) {
+    var first = first;
+    var last = last;
+        while (first != last && first.next != last) {
+        first = first.next;
+        last = last.prev;
+    }
+    return first;
+}
+
+function merge(leftPart, rightPart) {
+    var auxilaryList = null;
+    var start;
+
+    while (leftPart != null && rightPart != null) {
+        if (leftPart.item < rightPart.item) {
+            if (auxilaryList == null) {
+                auxilaryList = {
+                    item: leftPart.item,
+                    next: null,
+                    prev: null
+                }
+                start = auxilaryList;
+            } else {
+                auxilaryList.next = {
+                    item: leftPart.item,
+                    next: null,
+                    prev: auxilaryList
+                }
+                auxilaryList = auxilaryList.next;
+            }
+            leftPart = leftPart.next;
         } else {
-            this._first.prev = node;
-            node.next = this._first;
-            this._first = node;
-        }
-    },
-
-    toString: function() {
-        var current = this._first;
-
-        while (current != null) {
-            console.log(current.item);
-            current = current.next;
+            if (auxilaryList == null) {
+                auxilaryList = {
+                    item: rightPart.item,
+                    next: null,
+                    prev: null
+                }
+                start = auxilaryList;
+            } else {
+                auxilaryList.next = {
+                    item: rightPart.item,
+                    next: null,
+                    prev: auxilaryList
+                }
+                auxilaryList = auxilaryList.next;
+            }
+            rightPart = rightPart.next;
         }
     }
+
+    while (leftPart == null && rightPart != null) {
+        if (auxilaryList == null) {
+            auxilaryList = {
+                item: rightPart.item,
+                next: null,
+                prev: null
+            }
+            start = auxilaryList;
+        } else {
+            auxilaryList.next = {
+                item: rightPart.item,
+                next: null,
+                prev: auxilaryList
+            }
+            auxilaryList = auxilaryList.next;
+        }
+        rightPart =rightPart.next;
+    }
+
+    while (rightPart == null && leftPart != null) {
+        if (auxilaryList == null) {
+            auxilaryList = {
+                item: leftPart.item,
+                next: null,
+                prev: null
+            }
+            start = auxilaryList;
+        } else {
+            auxilaryList.next = {
+                item: leftPart.item,
+                next: null,
+                prev: auxilaryList
+            }
+            auxilaryList = auxilaryList.next;
+        }
+        leftPart = leftPart.next;
+    }
+
+    return start;
 }
 
-var ll = new LinkedList();
-ll.add('Some');
-ll.add('thing');
-ll.add('is');
-ll.add('wrong');
-ll.toString();
+function print(node) {
+    while (node != null) {
+        console.log(node.item);
+        node = node.next;
+    }
+}

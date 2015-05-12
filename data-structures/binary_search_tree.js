5b2c804b636525f6486939bf1b76abf981a46b2a
aks- <coderatlabs@gmail.com>
2015-05-12 20:01:29 +0530

    Add delete method for BST

--- data-structures/binary_search_tree.js
+++ data-structures/binary_search_tree.js
@@ -100,6 +100,82 @@ BST.prototype = {
     },
 
     delete: function(item) {
+        var found = false,
+            current = this._root,
+            parent = null;
+
+        while (!found) {
+            if (current.item > item) {
+                if (current.left != null) {
+                    parent = current;
+                    current = current.left;
+                } else {
+                    break;
+                }
+            } else if (current.item < item) {
+                if (current.right != null) {
+                    parent = current;
+                    current = current.right;
+                } else {
+                    break;
+                }
+            } else {
+                found = true;
+            }
+        }
+
+        if (found) {
+            var childCount = (current.left == null ? 0 : 1) + (current.right == null ? 0 : 1);
+            var replacement = null;
+            var replacementParent = null;
+
+            switch(childCount) {
+            case 0:
+                if (current == this._root) {
+                    this._root = null;
+                } else {
+                    parent.left == current ? parent.left = null : parent.right = null;
+                }
+                break;
+            case 1:
+                if (current == this._root) {
+                    this._root = current.left == null ? current.right : current.left;
+                } else {
+                    parent = parent.left == current ?
+                        (parent.left = (current.left == null? current.right : current.left))
+                    : (parent.right = (current.left == null? current.right : current.left));
+                }
+                break;
+            case 2:
+                replacement = current.left;
+                while (replacement.right != null) {
+                    replacementParent = replacement;
+                    replacement = replacement.right;
+                }
+                if (replacementParent == null) {
+                    if (current == this._root) {
+                        replacement.right = current.right;
+                        this._root = replacement;
+                    }
+                    else {
+                        var isCurrentLeftChild = parent.left == current ? true : false;
+                        replacement.right = current.right;
+                        isCurrentLeftChild ? parent.left = replacement : parent.right = replacement;
+                    }
+                } else {
+                    replacementParent.right = replacement.left;
+                    replacement.left = current.left;
+                    replacement.right = current.right;
+                    current == this._root ?
+                        this._root = replacement
+                        : (parent.left == current ? parent.left = replacement : parent.right = replacement)
+                }
+                break;
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
